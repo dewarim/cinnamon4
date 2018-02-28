d645693ec613c7f9e3956beb3148b4abbffede37
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-02-28 22:45:23 +0100

    Close #17: add mybatis acl mapping

---
+++ cinnamon-server/src/main/java/com/dewarim/cinnamon/model/Acl.java
@@ -0,0 +1,49 @@
+package com.dewarim.cinnamon.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.util.Objects;
+
+@JacksonXmlRootElement(localName = "acl")
+public class Acl {
+    
+    private Long id;
+    private String name;
+
+    public Acl() {
+    }
+
+    public Acl(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Acl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acl acl = (Acl) o;
+        return Objects.equals(name, acl.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
+}
