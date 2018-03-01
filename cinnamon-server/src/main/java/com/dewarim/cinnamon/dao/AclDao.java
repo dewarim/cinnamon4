0d72ba73d5360aaabcc5302c0812feef14e104aa
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-03-01 22:58:03 +0100

    Close #18: add AclDao.

---
+++ cinnamon-server/src/main/java/com/dewarim/cinnamon/dao/AclDao.java
@@ -0,0 +1,35 @@
+package com.dewarim.cinnamon.dao;
+
+import com.dewarim.cinnamon.application.ThreadLocalSqlSession;
+import com.dewarim.cinnamon.model.Acl;
+import org.apache.ibatis.session.SqlSession;
+
+public class AclDao {
+
+    public Acl getAclById(long id) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        return sqlSession.selectOne("com.dewarim.cinnamon.AclMaper.getAclById", id);
+    }
+
+    public Acl getAclByName(String name) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        return sqlSession.selectOne("com.dewarim.cinnamon.AclMaper.getAclByName", name);
+    }
+
+    public Acl save(Acl acl) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        sqlSession.insert("com.dewarim.cinnamon.AclMapper.insertAcl", acl);
+        return acl;
+    }
+
+    public void changeAclName(Acl acl) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        sqlSession.update("com.dewarim.cinnamon.AclMapper.changeAclName", acl);
+    }
+
+    public void deleteAcl(long id) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        sqlSession.delete("com.dewarim.cinnamon.AclMapper.deleteAcl", id);
+    }
+
+}
