018c2cf87766ee49bd13582a9a0061c7f65535f5
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-04-14 12:39:26 +0200

    Add Folder, FolderDao, FolderMapper Close #47

---
+++ cinnamon-server/src/main/java/com/dewarim/cinnamon/dao/FolderDao.java
@@ -0,0 +1,38 @@
+package com.dewarim.cinnamon.dao;
+
+import com.dewarim.cinnamon.application.ThreadLocalSqlSession;
+import com.dewarim.cinnamon.model.Folder;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.*;
+
+public class FolderDao {
+
+    /**
+     * Max number of ids in "in clause" is 32768 for Postgresql.
+     */
+    private static final int BATCH_SIZE = 10000;
+
+    // note: same code as OsdDao, although you could argue that fetching > 10K folders is a pathological case. 
+    public List<Folder> getFoldersById(List<Long> ids, boolean includeSummary) {
+        SqlSession sqlSession = ThreadLocalSqlSession.getSqlSession();
+        List<Folder> results = new ArrayList<>(ids.size());
+        int requestSize = ids.size();
+        int rowCount = 0;
+        Map<String,Object> params = new HashMap<>();
+        params.put("includeSummary", includeSummary);
+        while (rowCount < requestSize) {
+            int lastIndex = rowCount + BATCH_SIZE;
+            if(lastIndex > requestSize){
+                lastIndex = requestSize;
+            }
+            List<Long> partialList = ids.subList(rowCount, lastIndex);
+            params.put("idList",partialList);
+            results.addAll(sqlSession.selectList("com.dewarim.cinnamon.FolderMapper.getFoldersById", params));
+            rowCount += BATCH_SIZE;
+        }
+        return results;
+    }
+
+
+}
---
+++ cinnamon-server/src/main/java/com/dewarim/cinnamon/model/Folder.java
@@ -0,0 +1,108 @@
+package com.dewarim.cinnamon.model;
+
+import java.util.Objects;
+
+public class Folder {
+    
+    private Long id;
+    private String name;
+    private Long objVersion;
+    private Long aclId;
+    private Long ownerId;
+    private Long parentId;
+    private Long typeId;
+    private Boolean metadataChanged;
+    private String summary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getObjVersion() {
+        return objVersion;
+    }
+
+    public void setObjVersion(Long objVersion) {
+        this.objVersion = objVersion;
+    }
+
+    public Long getAclId() {
+        return aclId;
+    }
+
+    public void setAclId(Long aclId) {
+        this.aclId = aclId;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public Boolean getMetadataChanged() {
+        return metadataChanged;
+    }
+
+    public void setMetadataChanged(Boolean metadataChanged) {
+        this.metadataChanged = metadataChanged;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Folder folder = (Folder) o;
+        return Objects.equals(name, folder.name) &&
+                Objects.equals(objVersion, folder.objVersion) &&
+                Objects.equals(aclId, folder.aclId) &&
+                Objects.equals(ownerId, folder.ownerId) &&
+                Objects.equals(parentId, folder.parentId) &&
+                Objects.equals(typeId, folder.typeId) &&
+                Objects.equals(metadataChanged, folder.metadataChanged) &&
+                Objects.equals(summary, folder.summary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, parentId);
+    }
+}
