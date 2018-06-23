ffefe4da2b306d5d04909d8f201363e547d319cc
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-06-23 13:33:24 +0200

    Set contentType and characterEncoding for error responses.

--- cinnamon-server/src/main/java/com/dewarim/cinnamon/application/ErrorResponseGenerator.java
+++ cinnamon-server/src/main/java/com/dewarim/cinnamon/application/ErrorResponseGenerator.java
@@ -8,28 +8,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ErrorResponseGenerator {
-    
+
     private static ObjectMapper xmlMapper = new XmlMapper();
-    
-    public static void generateErrorMessage(HttpServletResponse response, int statusCode, ErrorCode errorCode, String message){
+
+    public static void generateErrorMessage(HttpServletResponse response, int statusCode, ErrorCode errorCode, String message) {
         CinnamonError error = new CinnamonError(errorCode.getCode(), message);
         try {
             response.setStatus(statusCode);
+            response.setContentType("application/xml");
+            response.setCharacterEncoding("UTF-8");
             xmlMapper.writeValue(response.getWriter(), error);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }    
-        
-    public static void generateErrorMessage(HttpServletResponse response, int statusCode, ErrorCode errorCode){
-        generateErrorMessage(response,statusCode,errorCode,null);
-    }    
-    
-    
-    public static void superuserRequired(HttpServletResponse response){
-        generateErrorMessage(response, HttpServletResponse.SC_FORBIDDEN,
-                ErrorCode.REQUIRES_SUPERUSER_STATUS, ""); 
     }
-    
+
+    public static void generateErrorMessage(HttpServletResponse response, int statusCode, ErrorCode errorCode) {
+        generateErrorMessage(response, statusCode, errorCode, null);
+    }
+
+
+    public static void superuserRequired(HttpServletResponse response) {
+        generateErrorMessage(response, HttpServletResponse.SC_FORBIDDEN,
+                ErrorCode.REQUIRES_SUPERUSER_STATUS, "");
+    }
+
 }
