703941ff4453fd89ee67a08f0c3eb09086a6fa0d
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-08-24 12:43:03 +0200

    Implements getFolder / getFolders unhappy path tests.
    Close #108

--- cinnamon-server/src/test/java/com/dewarim/cinnamon/test/integration/FolderServletIntegrationTest.java
+++ cinnamon-server/src/test/java/com/dewarim/cinnamon/test/integration/FolderServletIntegrationTest.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
@@ -48,7 +49,7 @@ public class FolderServletIntegrationTest extends CinnamonIntegrationTest {
     public void setSummaryMissingObject() throws IOException {
         SetSummaryRequest summaryRequest = new SetSummaryRequest(Long.MAX_VALUE, "a summary");
         HttpResponse      response       = sendStandardRequest(UrlMapping.FOLDER__SET_SUMMARY, summaryRequest);
-        assertCinnamonError(response, ErrorCode.OBJECT_NOT_FOUND, HttpStatus.SC_NOT_FOUND);
+        assertCinnamonError(response, ErrorCode.OBJECT_NOT_FOUND, SC_NOT_FOUND);
     }
 
     @Test
@@ -84,12 +85,41 @@ public class FolderServletIntegrationTest extends CinnamonIntegrationTest {
     }
 
     @Test
+    public void getFolderInvalidRequest() throws IOException {
+        SingleFolderRequest singleRequest = new SingleFolderRequest(0L, false);
+        HttpResponse        response      = sendStandardRequest(UrlMapping.FOLDER__GET_FOLDER, singleRequest);
+        assertCinnamonError(response, ErrorCode.INVALID_REQUEST);
+    }
+
+    @Test
+    public void getUnknownFolder() throws IOException {
+        SingleFolderRequest singleRequest = new SingleFolderRequest(Long.MAX_VALUE, false);
+        HttpResponse        response      = sendStandardRequest(UrlMapping.FOLDER__GET_FOLDER, singleRequest);
+        assertCinnamonError(response, ErrorCode.OBJECT_NOT_FOUND, SC_NOT_FOUND);
+    }
+
+    @Test
     public void getFoldersHappyPath() throws IOException {
         FolderRequest folderRequest = new FolderRequest(Arrays.asList(6L, 2L), false);
         HttpResponse  response      = sendStandardRequest(UrlMapping.FOLDER__GET_FOLDERS, folderRequest);
         List<Folder>  folders       = unwrapFolders(response, 2);
         assertTrue(folders.stream().anyMatch(folder -> folder.getId().equals(6L)));
         assertTrue(folders.stream().anyMatch(folder -> folder.getId().equals(2L)));
+    }
+
+    @Test
+    public void getFoldersInvalidRequest() throws IOException {
+        FolderRequest folderRequest = new FolderRequest(Collections.singletonList(0L), false);
+        HttpResponse  response      = sendStandardRequest(UrlMapping.FOLDER__GET_FOLDERS, folderRequest);
+        assertCinnamonError(response, ErrorCode.INVALID_REQUEST);
+    }
+
+
+    @Test
+    public void getUnknownFolders() throws IOException {
+        FolderRequest folderRequest = new FolderRequest(Collections.singletonList(Long.MAX_VALUE), false);
+        HttpResponse  response      = sendStandardRequest(UrlMapping.FOLDER__GET_FOLDERS, folderRequest);
+        assertCinnamonError(response, ErrorCode.OBJECT_NOT_FOUND, SC_NOT_FOUND);
     }
 
     private List<Folder> unwrapFolders(HttpResponse response, Integer expectedSize) throws IOException {
