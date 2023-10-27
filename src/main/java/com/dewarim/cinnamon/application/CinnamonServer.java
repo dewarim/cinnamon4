7b9301fe001b5b58f7055c5907299ea7ebadcbf1
Ingo Wiarda <ingo_wiarda@dewarim.de>
2023-10-27 16:38:22 +0200

    version++ to match next tag

--- src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
+++ src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
@@ -59,7 +59,7 @@ public class CinnamonServer {
 
     private static final Logger log = LogManager.getLogger(CinnamonServer.class);
 
-    public static final String           VERSION       = "0.6.2";
+    public static final String           VERSION       = "0.6.3";
     private             Server           server;
     private             DbSessionFactory dbSessionFactory;
     private final       WebAppContext    webAppContext = new WebAppContext();
