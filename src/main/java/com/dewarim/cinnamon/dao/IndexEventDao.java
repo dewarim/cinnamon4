b7e0e0c1162eeb642f60281c4eea848dfa482a35
Ingo Wiarda <ingo_wiarda@dewarim.de>
2025-04-26 00:08:39 +0200

    add custom session handling for IndexEventDao

--- src/main/java/com/dewarim/cinnamon/dao/IndexEventDao.java
+++ src/main/java/com/dewarim/cinnamon/dao/IndexEventDao.java
@@ -5,6 +5,20 @@ import org.apache.ibatis.session.SqlSession;
 
 public class IndexEventDao implements CrudDao<IndexEvent> {
 
+    private SqlSession sqlSession;
+
+    public IndexEventDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    @Override
+    public SqlSession getSqlSession() {
+        if (sqlSession == null) {
+            sqlSession = CrudDao.super.getSqlSession();
+        }
+        return sqlSession;
+    }
+
     public void commit() {
         SqlSession sqlSession = getSqlSession();
         sqlSession.commit();
