df198babce8d3891641c34bdfa7710780a9af427
Ingo Wiarda <ingo_wiarda@dewarim.de>
2025-04-26 10:08:01 +0200

    version++

--- src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
+++ src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
@@ -62,7 +62,7 @@ public class CinnamonServer {
 
     private static final Logger log = LogManager.getLogger(CinnamonServer.class);
 
-    public static final String           VERSION       = "1.6.4";
+    public static final String           VERSION       = "1.6.5";
     private             Server           server;
     private             DbSessionFactory dbSessionFactory;
     private final       WebAppContext    webAppContext = new WebAppContext();
