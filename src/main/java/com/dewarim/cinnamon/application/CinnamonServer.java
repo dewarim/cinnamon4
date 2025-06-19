1e15924c7c41d8c35a88226d77792a3e381f0db6
Ingo Wiarda <ingo_wiarda@dewarim.de>
2025-06-19 12:58:00 +0200

    increase version to 1.7.0

--- src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
+++ src/main/java/com/dewarim/cinnamon/application/CinnamonServer.java
@@ -62,7 +62,7 @@ public class CinnamonServer {
 
     private static final Logger log = LogManager.getLogger(CinnamonServer.class);
 
-    public static final String           VERSION       = "1.6.9";
+    public static final String           VERSION       = "1.7.0";
     private             Server           server;
     private             DbSessionFactory dbSessionFactory;
     private final       WebAppContext    webAppContext = new WebAppContext();
